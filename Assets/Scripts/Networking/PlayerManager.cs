06f4a98aa00741038f58b6feba1473c71e640c36
panda-hackerman <58387096+panda-hackerman@users.noreply.github.com>
2020-10-06 13:26:33 -0700

    Networking
    Added some networking stuff including scene stuff

---
+++ Assets/Scripts/Networking/PlayerManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerManager : NetworkBehaviour
+{
+    //TODO: Replace with a list of Card objects
+    public List<GameObject> cards = new List<GameObject>();
+
+    public override void OnStartClient()
+    {
+        base.OnStartClient();
+        //Server and client will do this (server acts as client)
+    }
+
+    [Server]
+    public override void OnStartServer()
+    {
+        base.OnStartServer();
+        //Only the server does this
+    }
+
+    [Command]
+    public void CmdDealCards()
+    {
+
+    }
+}
