d4bdeab5b63a642b1a32dba8ca92602d631304f7
SomeGreenDude <70672419+SomeGreenDude@users.noreply.github.com>
2021-02-07 21:32:56 -0800

    BIGGEST UPDATE EVVERRRR
    Profile Images save (:

--- Assets/Scripts/Tools/ImageImportAndScaler.cs
+++ Assets/Scripts/Tools/ImageImportAndScaler.cs
@@ -9,8 +9,8 @@ using SFB;
 
 public class ImageImportAndScaler : MonoBehaviour, IPointerDownHandler
 {
-    //Put cool image you want to change in the inspector
-    public Toggle toggle; //optional
+    // Put cool image you want to change in the inspector
+    public Toggle toggle; // optional
     public RawImage output;
     public int resX = 70;
     public int resY = 70;
@@ -22,6 +22,14 @@ public class ImageImportAndScaler : MonoBehaviour, IPointerDownHandler
         // Listens to button input without dragging it in
         var button = GetComponent<Button>();
         button.onClick.AddListener(OnClick);
+
+        // Checks for the last saved image
+        Texture2D setupTexture = ReadTextureFromPlayerPrefs("LastImage");
+        if (setupTexture != null)
+        {
+            SetImageOutput(setupTexture);
+        }
+        else Debug.Log("No image found");
     }
 
     public void OnClick()
@@ -41,11 +49,41 @@ public class ImageImportAndScaler : MonoBehaviour, IPointerDownHandler
         yield return www.SendWebRequest();
         
         Texture2D newTexture = DownloadHandlerTexture.GetContent(www); // I am the image
-        TextureScale.Bilinear(newTexture, resX, resY); // Change resolution
+        SetImageOutput(newTexture); // Ourrr image
         
-        output.texture = newTexture.ChangeFormat(TextureFormat.RGB565); // It's ourrr image
+        // Convert texture to bytes for a PNG
+        byte[] imageBytes = newTexture.EncodeToPNG();
+        // Turn bytes into a string
+        string newTextureBytes = System.Convert.ToBase64String(imageBytes);
+        // Saves the string in PlayerPrefs
+        PlayerPrefs.SetString("LastImage", newTextureBytes);
 
         if (toggle) toggle.isOn = true; //Select it
+    }
+
+    public static Texture2D ReadTextureFromPlayerPrefs(string tag)
+    {
+        // Load string from playerpref
+        string imageBytes = PlayerPrefs.GetString(tag, null);
+        // Checks if the string isn't empty
+        if (!string.IsNullOrEmpty(imageBytes))
+        {
+            Debug.Log("Last Image Found!");
+
+            // Convert back into byte array
+            byte[] TextureBytes = System.Convert.FromBase64String(imageBytes);
+            // Scaling
+            Texture2D tex = new Texture2D(2, 2);
+
+            // If all goes well than load the texture and return
+            if (tex.LoadImage(TextureBytes)) return tex;
+        }
+        return null;
+    }
+    public void SetImageOutput(Texture2D TextureToOutput)
+    {
+        TextureScale.Bilinear(TextureToOutput, resX, resY); // Change resolution
+        output.texture = TextureToOutput.ChangeFormat(TextureFormat.RGB565); // Change format and set output
     }
 }
 
